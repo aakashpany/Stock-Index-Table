fc3b53de01f4f331a312311815028476e042aa9e
aakashpany <80850296+aakashpany@users.noreply.github.com>
2021-07-21 11:02:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MerQube',
+    version='',
+    packages=[''],
+    url='merqube.com',
+    license='',
+    author='Aakash Pany',
+    author_email='aakash.pany@gmail.com',
+    description=''
+)
 
